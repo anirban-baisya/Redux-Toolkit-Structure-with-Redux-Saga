6075c274855a8548a5c73c3b065732a88dc41341
Anirban Baisya <88102358+anirban-baisya@users.noreply.github.com>
2022-06-28 12:53:03 +0530

    Update loginSlicer.js

--- redux/slicers/loginSlicer.js
+++ redux/slicers/loginSlicer.js
@@ -6,7 +6,7 @@ export const loginSlicer = createSlice({ //whole slicer
     initialState: {
         isLoading: false,
         isError: false,
-        data: {}, //it's store all the respons from the api
+        data: {},  //it's store all the respons from the api ; if api respnose is comming in the from of array then i have to give [] & i have to change the thing in   onGetallCategoriesSubmit: (state) => { data: [] } ; other wise if the response comming in the from of {} then i have to change it in obj.
         error: {}
     },
     reducers: { //different actions
