5c6b96af81484cda9d129f2af46e63765d73911b
Anirban Baisya <88102358+anirban-baisya@users.noreply.github.com>
2022-06-28 13:43:11 +0530

    Update loginSaga.js

--- redux/sagas/loginSaga.js
+++ redux/sagas/loginSaga.js
@@ -5,7 +5,7 @@ import { callLoginApi } from './../../services/loginApi';
 function* doUserExists({ payload }) {
     // const { query } = payload;
     // console.log(query)
-    if (payload) { //if i have to send any query , or want to send any data then i have to pass the paylod aloso  yield call(callgetAllCategoriesApi,payload)
+    if (payload) { //if i have to send any query , or want to send any data then i have to pass the paylod aloso  yield call(callgetAllCategoriesApi,payload) ; other wise i have to comment this section & have to remove payload from below
         try {
             const userExistResponse = yield call(callLoginApi, payload);
             console.log("-=-=-= >>> ", userExistResponse) //after receving response from api we have to call the onUserExistSubmitSuccess(userExistResponse) action 
